605e8644a3d46a4c9f8dcd9c15b8c093409d2950
ehmicky <ehmicky@gmail.com>
2025-05-18 14:12:39 +0100

    Initial commit

--- src/main.test-d.ts
+++ src/main.test-d.ts
@@ -1,8 +1,100 @@
-import { expectAssignable, expectType } from 'tsd'
+import { expectAssignable, expectNotAssignable, expectType } from 'tsd'
 
-import switchFunctional, { type Options } from 'switch-functional'
+import switchFunctional, {
+  type Condition,
+  type Switch,
+} from 'switch-functional'
 
-expectType<unknown>(switchFunctional(true))
+const switchStatement = switchFunctional(true)
+expectType<Switch>(switchStatement)
 
+// @ts-expect-error
+switchFunctional()
+// @ts-expect-error
 switchFunctional(true, {})
-expectAssignable<Options>({})
+
+switchFunctional(0)
+switchFunctional(0n)
+switchFunctional(true)
+switchFunctional(null)
+switchFunctional(undefined)
+switchFunctional('')
+switchFunctional(Symbol(''))
+switchFunctional({} as const)
+switchFunctional([] as const)
+switchFunctional(() => {})
+
+expectType<Switch>(switchStatement.case(true, true))
+
+switchStatement.case(0, true)
+switchStatement.case(0n, true)
+switchStatement.case(true, true)
+switchStatement.case(null, true)
+switchStatement.case(undefined, true)
+switchStatement.case('', true)
+switchStatement.case(Symbol(''), true)
+switchStatement.case({} as const, true)
+switchStatement.case({ a: true } as const, true)
+switchStatement.case({ a: () => '' } as const, true)
+switchStatement.case([] as const, true)
+switchStatement.case([true] as const, true)
+switchStatement.case([() => ''] as const, true)
+switchStatement.case(() => true, true)
+expectAssignable<Condition>(0)
+expectAssignable<Condition>(0n)
+expectAssignable<Condition>(true)
+expectAssignable<Condition>(null)
+expectAssignable<Condition>(undefined)
+expectAssignable<Condition>('')
+expectAssignable<Condition>(Symbol(''))
+expectAssignable<Condition>({} as const)
+expectAssignable<Condition>({ a: true } as const)
+expectAssignable<Condition>({ a: () => '' } as const)
+expectAssignable<Condition>([] as const)
+expectAssignable<Condition>([true] as const)
+expectAssignable<Condition>([() => ''] as const)
+expectAssignable<Condition>(() => true)
+
+// @ts-expect-error
+switchStatement.case(() => '', true)
+expectNotAssignable<Condition>(() => '')
+
+switchStatement.case(true, 0)
+switchStatement.case(true, 0n)
+switchStatement.case(true, true)
+switchStatement.case(true, null)
+switchStatement.case(true, undefined)
+switchStatement.case(true, '')
+switchStatement.case(true, Symbol(''))
+switchStatement.case(true, {} as const)
+switchStatement.case(true, [] as const)
+switchStatement.case(true, () => {})
+
+// @ts-expect-error
+switchStatement.case()
+// @ts-expect-error
+switchStatement.case(true)
+// @ts-expect-error
+switchStatement.case(true, true, true)
+
+expectAssignable<unknown>(switchStatement.default(true))
+
+switchStatement.default(0)
+switchStatement.default(0n)
+switchStatement.default(true)
+switchStatement.default(null)
+switchStatement.default(undefined)
+switchStatement.default('')
+switchStatement.default(Symbol(''))
+switchStatement.default({} as const)
+switchStatement.default([] as const)
+switchStatement.default(() => {})
+
+// @ts-expect-error
+switchStatement.default()
+// @ts-expect-error
+switchStatement.default(true, true)
+
+// @ts-expect-error
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+switchStatement.other()
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,108 @@
+// Functional `switch` statement
+const chain = (resolved: Resolved, value: Value): Switch => ({
+  case: addCase.bind(undefined, { resolved, value }),
+  default: useDefault.bind(undefined, { resolved, value }),
+})
+
+export interface Switch {
+  case: (conditions: Conditions, effect: Effect) => Switch
+  default: (effect: Effect) => unknown
+}
+
+// `switchFunctional(value)[.case(...)].case(conditions, effect)`
+const addCase = (
+  { resolved, value }: Context,
+  conditions: Conditions,
+  effect: Effect,
+) =>
+  resolved || !matchesConditions(value, conditions)
+    ? chain(resolved, value)
+    : chain(true, applyEffect(value, effect))
+
+// `switchFunctional(value)[.case()...].default(effect)`
+const useDefault = ({ resolved, value }: Context, effect: Effect) =>
+  resolved ? value : applyEffect(value, effect)
+
+const matchesConditions = (value: Value, conditions: Conditions) =>
+  Array.isArray(conditions)
+    ? (conditions as Condition[]).some((condition) =>
+        matchesCondition(value, condition),
+      )
+    : matchesCondition(value, conditions)
+
+const matchesCondition = (value: Value, condition: Condition) => {
+  if (typeof condition === 'function') {
+    return condition(value)
+  }
+
+  if (typeof condition === 'boolean') {
+    return condition
+  }
+
+  return deepIncludes(value, condition)
+}
+
+// Check for deep equality. For objects (not arrays), check if deep superset.
+const deepIncludes = (value: Value, subset: unknown): boolean => {
+  if (
+    !isObject(value) ||
+    !isObject(subset) ||
+    Array.isArray(value) !== Array.isArray(subset)
+  ) {
+    return Object.is(value, subset)
+  }
+
+  if (Array.isArray(value) && Array.isArray(subset)) {
+    return (
+      subset.length === value.length &&
+      subset.every((item, index) => deepIncludes(value[index], item))
+    )
+  }
+
+  return Object.entries(subset).every(([name, child]) =>
+    deepIncludes(value[name], child),
+  )
+}
+
+const isObject = (value: Value): value is { [name: PropertyKey]: unknown } =>
+  typeof value === 'object' && value !== null
+
+const applyEffect = (value: Value, effect: Effect): unknown =>
+  typeof effect === 'function' ? (effect as FunctionEffect)(value) : effect
+
 /**
  *
  * @example
  * ```js
  * ```
  */
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const switchFunctional = (value: unknown, options?: Options): unknown => value
+const switchFunctional = chain.bind(undefined, false)
 
 export default switchFunctional
 
-/**
- * `switch-functional` options
- */
-// eslint-disable-next-line @typescript-eslint/no-empty-interface, @typescript-eslint/no-empty-object-type
-export interface Options {}
+type Resolved = boolean
+
+type Value = unknown
+
+interface Context {
+  readonly resolved: Resolved
+  readonly value: Value
+}
+
+type Conditions = Condition | readonly Condition[]
+
+export type Condition =
+  | string
+  | number
+  | boolean
+  | bigint
+  | symbol
+  | null
+  | undefined
+  | readonly unknown[]
+  | { readonly [key: PropertyKey]: unknown }
+  | ((value: unknown) => boolean)
+
+type Effect = unknown
+
+type FunctionEffect = (value: unknown) => unknown
